8be29106f5d2a0c2ae2f420c5712b42b19b6a7ff
cromeo98 <romeo.christian98@gmail.com>
2021-09-15 16:12:09 +0200

    basic page structure

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">this is one footer element</div>
+            <div class="col-3">this is one footer element</div>
+            <div class="col-3">this is one footer element</div>
+            <div class="col-3">this is one footer element</div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">this is one header element</div>
+            <div class="col-3">this is one header element</div>
+            <div class="col-3">this is one header element</div>
+            <div class="col-3">this is one header element</div>
+        </div>
+    </div>
+</header>
